28b27241a16fc26f3b1fc6579b150a9b12cf8746
Daniela <daniela.arroyo27@gmail.com>
2020-02-20 20:38:29 -0600

    error

--- js/daniTesting.js
+++ js/daniTesting.js
@@ -10,7 +10,7 @@ fetch(`https://developers.zomato.com/api/v2.1/geocode?lat=${lat}&lon=${lon}`, {
     })
     .then((res_array) => {
         let arrayOfNearby = res_array.nearby_restaurants
-        console.log(res_array.nearby_restaurants)
+        // console.log(res_array.nearby_restaurants)
         let $listGroupContainer = $('.list-group');
         let liTags = arrayOfNearby.map((e) => {
             return `<a class='list-group-item list-group-item-action' href="${e.restaurant.url}">${e.restaurant.name}</a>`
@@ -18,7 +18,11 @@ fetch(`https://developers.zomato.com/api/v2.1/geocode?lat=${lat}&lon=${lon}`, {
         $listGroupContainer.html(liTags.join(''));
     })
 
+
 $div = $(".list-group");
 $div.click((e) => {
-    alert('ouch')
+    e.preventDefault();
+    console.log(e.target.href)
+    fetch(e.target.href)
+
 })
