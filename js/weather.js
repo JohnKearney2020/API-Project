d8435ed18d58ce4933c893d983b7d6c6c7519923
Daniela <daniela.arroyo27@gmail.com>
2020-02-25 15:41:31 -0600

    weather temp

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,36 @@
-fetch(`https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/34983279487e255ca75000221c62bb9f/29.7559698,-95.3573194`)
-    .then((response) => {
-        return response.json();
-    })
-    .then((res_array) => {
-        console.log(res_array)
-    })
+let weatherCont = document.querySelector('#whiteOnWhite')
+function getWeather() {
+    fetch(`https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/34983279487e255ca75000221c62bb9f/29.7559698,-95.3573194`)
+        .then((response) => {
+            return response.json();
+        })
+        .then((res_array) => {
+            console.log(res_array)
+            let arrayCurrent = res_array.currently
+            let currentTemp = arrayCurrent.temperature + ' F'
+            let currentSum = arrayCurrent.summary
+            let currentIcon = arrayCurrent.icon
+            console.log(currentTemp)
+            console.log(currentSum)
+            console.log(currentIcon)
+            return `${currentSum}`
+
+        })
+        .then((result) => {
+            weatherCont.innerHTML = result
+        })
+}
+getWeather()
+// var skycons = new Skycons({ "color": "pink" });
+// var icons = new Skycons(),
+// list  = [
+//   "clear-day", "clear-night", "partly-cloudy-day",
+//   "partly-cloudy-night", "cloudy", "rain", "sleet", "snow", "wind",
+//   "fog"
+// ],
+// i;
+
+// for(i = list.length; i--; )
+// icons.set(list[i], list[i]);
+
+// icons.play();
