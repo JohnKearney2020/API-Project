0a27a3010280eefd46942f907f326ffcf4ea3e9e
Daniela <daniela.arroyo27@gmail.com>
2020-02-25 18:08:03 -0600

    final weather

--- js/weather.js
+++ js/weather.js
@@ -17,20 +17,43 @@ window.addEventListener("load", () => {
                 })
                 .then(data => {
                     console.log(data)
-                    const { temperature, summary, icon } = data.currently
+                    const { temperature, summary, icon, precipProbability, apparentTemperature } = data.currently
                     // set DOM elements from the api
                     tempCont.textContent = temperature
-                    sumCont.textContent = `Today: ${summary} currently.`
+                    sumCont.textContent = `Today: ${summary} currently. With ${precipProbability}% chance of rain. Feels ${apparentTemperature}`
                     // iconCont.innerHTML = icon
                     switch (icon) {
-                        case "clear-day":
+                        case "clear-night":
+                            iconCont.innerHTML = '<img src="images/icons-weather/clear-night.png">'
+                            break;
+                        case ("clear-day"):
                             console.log("its clear day")
-
+                            iconCont.innerHTML = '<img src="images/icons-weather/clear-day.png">'
                             break;
-                        case ("clear-night"):
-                            console.log("its clear night")
+                        case ("cloudy"):
+                            iconCont.innerHTML = '<img src="images/icons-weather/cloudy.png">'
                             break;
-
+                        case ("fog"):
+                            iconCont.innerHTML = '<img src="images/icons-weather/fog.png">'
+                            break;
+                        case ("partly-cloudy-day"):
+                            iconCont.innerHTML = '<img src="images/icons-weather/partly-cloudy-day.png">'
+                            break;
+                        case ("partly-cloudy-night"):
+                            iconCont.innerHTML = '<img src="images/icons-weather/partly-cloudy-night.png">'
+                            break;
+                        case ("rain"):
+                            iconCont.innerHTML = '<img src="images/icons-weather/rain.png">'
+                            break;
+                        case ("sleet"):
+                            iconCont.innerHTML = '<img src="images/icons-weather/sleet.png">'
+                            break;
+                        case ("snow"):
+                            iconCont.innerHTML = '<img src="images/icons-weather/snow.png">'
+                            break;
+                        case ("wind"):
+                            iconCont.innerHTML = '<img src="images/icons-weather/wind.png">'
+                            break;
                     }
                 })
         });
